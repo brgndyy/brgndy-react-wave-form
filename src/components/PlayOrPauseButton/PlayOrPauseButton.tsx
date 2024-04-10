97a4325e321287ba36b199db9ac04ae34580c229
brgndy <brgndy96@gmail.com>
2024-04-10 21:48:41 +0900

    feat: 재생 or 일시정지 버튼 생성

---
+++ src/components/PlayOrPauseButton/PlayOrPauseButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useAudio } from "../Provider/AudioWaveFormWrapper";
+
+type PlayOrPauseButtonProps = {
+  src: string;
+  selectSongHandler: (currentMusicSrc: string) => void;
+  className: string;
+  playOrPauseValues: [string, string];
+};
+
+export default function PlayOrPauseButton({
+  src,
+  selectSongHandler,
+  className,
+  playOrPauseValues,
+}: PlayOrPauseButtonProps) {
+  const { isPlaying, currentSrc } = useAudio();
+
+  return (
+    <>
+      <button onClick={() => selectSongHandler(src)} className={className}>
+        {isPlaying && src === currentSrc
+          ? playOrPauseValues[1]
+          : playOrPauseValues[0]}
+      </button>
+    </>
+  );
+}
