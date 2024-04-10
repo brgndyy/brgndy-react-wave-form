2ef3f0fbbef985a68b5ca9621037bb5d262b5bea
brgndy <brgndy96@gmail.com>
2024-04-10 21:48:18 +0900

    feat: cavas 컴포넌트 생성

---
+++ src/components/WaveFormCanvas/WaveFormCanvas.tsx
@@ -0,0 +1,34 @@
+import React, { RefObject } from "react";
+
+type WaveformCanvasPropsType = {
+  canvasRef: RefObject<HTMLCanvasElement> | null;
+  clickCanvasProgressBarHandler: (
+    event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => void;
+  className: string;
+  width: number;
+  height: number;
+};
+
+export default function WaveFormCanvas({
+  canvasRef,
+  clickCanvasProgressBarHandler,
+  className = "",
+  width,
+  height,
+}: WaveformCanvasPropsType) {
+  return (
+    <>
+      <canvas
+        style={{
+          cursor: "pointer",
+        }}
+        ref={canvasRef}
+        width={width}
+        height={height}
+        className={className}
+        onClick={clickCanvasProgressBarHandler}
+      ></canvas>
+    </>
+  );
+}
